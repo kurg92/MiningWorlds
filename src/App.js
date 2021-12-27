8795d1b8f31b7f5e517d71ed41d2e65c1200b34b
kurg92 <perrot.tristan.pro@gmail.com>
2021-12-27 17:43:06 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -78,10 +78,10 @@ export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
   border: 4px dashed var(--secondary);
   background-color: var(--accent);
-  border-radius: 100%;
+  border-radius: 5%;
   width: 200px;
   @media (min-width: 900px) {
-    width: 250px;
+    width: 300px;
   }
   @media (min-width: 1000px) {
     width: 300px;
